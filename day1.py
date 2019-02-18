ba23e05b2d6e9b45a29c5f41d8a7901b64c6bc06
yuidea <35512442+yuishizhume@users.noreply.github.com>
2019-02-18 16:34:36 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,4 @@
-
+print 'The quick brown fox', 'jumps over', 'the lazy dog'
+name = raw_input('Please enter your name:')
+point = input('Please enter your point:')
+print 'Hello',name,'Your point is:',point
