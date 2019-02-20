37800369e581b5fd8708af1b0b1ceed334debbed
yuidea <35512442+yuishizhume@users.noreply.github.com>
2019-02-20 10:44:22 +0800

    finnal change of day2's all

--- day2.2.py
+++ day2.2.py
@@ -100,4 +100,57 @@ L = ['Hello', 'World', 'IBM', 'Apple']
 #小结
 #运用列表生成式，可以快速生成list，可以通过一个list推导出另一个list，而代码却十分简洁。
 
+#生成器
+#受到内存限制，列表容量肯定是有限的
+#如果我们仅仅需要访问前面几个元素，那后面绝大多数元素占用的空间都白白浪费了
+#如果列表元素可以按照某种算法推算出来，那我们是否可以在循环的过程中不断推算出后续的元素呢？
+#这样就不必创建完整的list，从而节省大量的空间。
+#在Python中，这种一边循环一边计算的机制，称为生成器（Generator）。
 
+#要创建一个generator，有很多种方法。
+#把一个列表生成式的[]改成()，就创建了一个generator：
+L = [x * x for x in range(10)]#list
+g = (x * x for x in range(10))#generator
+L,g#<generator object <genexpr> at 0x0000000009B992D0>
+#怎么打印出generator的每一个元素呢？
+#可以通过generator的next()方法
+g.next()
+#generator保存的是算法，每次调用next()，就计算出下一个元素的值，直到计算到最后一个元素，没有更多的元素时，抛出StopIteration的错误。
+#正确的方法是使用for循环
+g = (x * x for x in range(10))
+for n in g:
+    print n
+#基本上永远不会调用next()方法，而是通过for循环来迭代它。
+#著名的斐波拉契数列（Fibonacci）
+#除第一个和第二个数外，任意一个数都可由前两个数相加得到：
+#1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print b
+        a, b = b, a + b
+        n = n + 1
+fib(6)
+#要把fib函数变成generator，
+#只需要把print b改为yield b就可以了：
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+fib(6).next()#<generator object fib at 0x0000000009B99360>
+#如果一个函数定义中包含yield关键字，那么这个函数就不再是一个普通函数，而是一个generator
+#这里，最难理解的就是generator和函数的执行流程不一样。
+#变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+#函数改成generator后，我们基本上从来不会用next()来调用它
+#而是直接使用for循环来迭代：
+for n in fib(6):
+    print n
+    
+'''
+小结：
+generator是非常强大的工具，在Python中，可以简单地把列表生成式改成generator，也可以通过函数实现复杂逻辑的generator。
+
+要理解generator的工作原理，它是在for循环的过程中不断计算出下一个元素，并在适当的条件结束for循环。对于函数改成的generator来说，遇到return语句或者执行到函数体最后一行语句，就是结束generator的指令，for循环随之结束。
+'''
